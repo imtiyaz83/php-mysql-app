69f0b97562a27492e2cfdb3ff9e4ddbfaf9b0a59
Imtiyaz Khan <imtiyaz83@users.noreply.github.com>
2024-09-12 16:23:28 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <h1>Yay! Deployed!</h1>
+    <h1>Yay!!! Deployed!</h1>
     <h1>Welcome to My PHP Website!!</h1>
 </body>
 </html>
