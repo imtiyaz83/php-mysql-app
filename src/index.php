667f38598270e9a76f6da3eca63ea942ef88c095
root <root@DESKTOP-0K1854S>
2024-09-12 11:45:04 +0530

    added the directory and files

---
+++ src/index.php
@@ -0,0 +1,13 @@
+// src/index.php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1>Welcome to My PHP Website!</h1>
+</body>
+</html>
+
