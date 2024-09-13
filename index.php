f3c735cdd5bd8d3d7d936e36a884ca44c6850080
Imtiyaz Khan <imtiyaz83@users.noreply.github.com>
2024-09-13 12:56:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<h1>Yes, Its working fine!!!!!!!!!!</h1>
+<h1>Yes, Its working fine</h1>
     <h1>Welcome to My PHP Application!!</h1>
 </body>
 </html>
