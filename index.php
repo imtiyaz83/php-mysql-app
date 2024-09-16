9c98f8f55bee39cf7694a07b5d5ce7e287b36e9d
Imtiyaz Khan <imtiyaazkhan@gmail.com>
2024-09-16 11:33:30 +0530

    File changed

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<h1>Yes, The CICD pipeline is working verified!!!</h1>
+<h1>This is my first CI/CD push push based approach project!</h1>
     <h1>Welcome to My PHP Application!!</h1>
 </body>
 </html>
