911e517ef55cb3244866e4876972f29f0754579f
Imtiyaz Khan <imtiyaazkhan@gmail.com>
2024-09-16 11:49:34 +0530

    File modified

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<h1>This is my first CI/CD push push based approach project!</h1>
+<h1>Yes, The CI/CD Pipeline is working fine!</h1>
     <h1>Welcome to My PHP Application!!</h1>
 </body>
 </html>
