d13300c084c819c826a63b9d5b7d1a39482e9c6a
Imtiyaz Khan <imtiyaazkhan@gmail.com>
2024-09-28 13:06:11 +0530

    Files modified

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<h1>Testing11</h1>
     <h1>Welcome to My PHP Application!!</h1>
+    <h2>Now, The applicaition is ready to use. Please pull the docker image and start wroking on it.</h2>
 </body>
 </html>
 
--- src/index.php
+++ src/index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome1111</title>
+    <title>Welcome!</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <h1>Yay! Now Its Working Fine!</h1>
-    <h1>Welcome to My PHP Website!!</h1>
+    <h1>Welcome to My PHP Application!!</h1>
+    <h2>Now, The applicaition is ready to use. Please pull the docker image and start wroking on it.</h2>
 </body>
 </html>
 
